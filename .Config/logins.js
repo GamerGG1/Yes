36d91f3a9d5f86cb7bb60d59d9fd75b6de1c47b5
GamerGG1 <50841876+GamerGG1@users.noreply.github.com>
2020-05-07 18:10:07 +0300

    Update logins.js

--- .Config/logins.js
+++ .Config/logins.js
@@ -1,7 +1,7 @@
 module.exports = {
   trello: {
-    token: "",
-    key: ""
+    token: "a488f7e81eb587b283a1acc9d813d74465f76fc94f92f4baa347c3bae9583370",
+    key: "a30fdb1c55b8462ae986727ede60bd3c"
   },
   redis: {
     host: "localhost",
