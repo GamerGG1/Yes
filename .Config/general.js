d027e97e1c7faa384b92cc698d7377c3415e6b1b
GamerGG1 <50841876+GamerGG1@users.noreply.github.com>
2020-05-07 18:21:38 +0300

    Update general.js

--- .Config/general.js
+++ .Config/general.js
@@ -1,10 +1,10 @@
 module.exports = {
-  token: "",
+  token: "NTg3NjQ3MzI2NjAwODIyNzk2.XrQmAg.nK7Ab_Y61wM3oeeT7eYpY_pgOXg",
   prefix: "T!",
   elevated: [""],
   commands: "./Commands",
   debug: false,
   embedColor: 4365262,
-  webhookURL: "",
+  webhookURL: "https://discordapp.com/api/webhooks/707975073985855598/jAdExp2rptCLwW62m2otsa6t8M3NEcTQMbK0j-fMqcFeBaWjehB4KepirTW9d8daGq7g",
   webhookBetaURL: ""
 };
